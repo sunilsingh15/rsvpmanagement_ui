81daecf6c348bad0f407fa55ac390428c24c7f1d
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-28 17:41:06 +0800

    api now supports retrieving all rsvps

--- src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPAPIController.java
+++ src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPAPIController.java
@@ -1,10 +1,28 @@
 package sg.sunilsingh.rsvpmanagement_ui.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sg.sunilsingh.rsvpmanagement_ui.model.RSVP;
 
 @RestController
 @RequestMapping("/api")
 public class RSVPAPIController {
+
+    @Autowired
+    JdbcTemplate template;
+
+    private final String getAllRSVPs = "select * from rsvps";
+
+    @GetMapping("/rsvps")
+    public List<RSVP> allRSVPs() {
+        return template.query(getAllRSVPs, BeanPropertyRowMapper.newInstance(RSVP.class));
+    }
     
 }
