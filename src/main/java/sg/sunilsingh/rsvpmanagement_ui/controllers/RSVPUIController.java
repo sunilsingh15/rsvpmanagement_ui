5af454f7ca7c003746c214437bfd935eaf8f65d2
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-28 17:11:15 +0800

    index page mapped

--- src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPUIController.java
+++ src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPUIController.java
@@ -1,10 +1,16 @@
 package sg.sunilsingh.rsvpmanagement_ui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class RSVPUIController {
+
+    @GetMapping
+    public String indexPage() {
+        return "index";
+    }
     
 }
