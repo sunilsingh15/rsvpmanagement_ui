48abedd958d79e14ecc7a016430f85d4407c8fbc
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-28 20:03:00 +0800

    added validation constraints

--- src/main/java/sg/sunilsingh/rsvpmanagement_ui/model/RSVP.java
+++ src/main/java/sg/sunilsingh/rsvpmanagement_ui/model/RSVP.java
@@ -2,6 +2,11 @@ package sg.sunilsingh.rsvpmanagement_ui.model;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +17,21 @@ import lombok.NoArgsConstructor;
 public class RSVP {
 
     private int id;
+
+    @Size(min = 3, message = "Name must have at least 3 characters!")
     private String fullName;
+
+    @NotEmpty(message = "E-mail cannot be empty!")
+    @Email
     private String email;
-    private int phone;
+
+    @Size(min = 8, max = 8, message = "Phone number must consist of 8 numbers!")
+    private String phone;
+
+    @NotNull(message = "Date cannot be empty!")
+    @FutureOrPresent(message = "RSVP date cannot be in the past!")
     private Date confirmationDate;
+
     private String comments;
-    
+
 }
