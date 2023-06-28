e4396d54d61614e4314f14aaf6a61b55d0fc6252
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-28 17:41:48 +0800

    updated ui controller for allrsvps button

--- src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPUIController.java
+++ src/main/java/sg/sunilsingh/rsvpmanagement_ui/controllers/RSVPUIController.java
@@ -1,16 +1,29 @@
 package sg.sunilsingh.rsvpmanagement_ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import sg.sunilsingh.rsvpmanagement_ui.service.RSVPUIService;
 
 @Controller
 @RequestMapping
 public class RSVPUIController {
 
+    @Autowired
+    RSVPUIService service;
+
     @GetMapping
     public String indexPage() {
         return "index";
+    }
+
+    @GetMapping("/view")
+    public String viewAllRSVPs(Model model) {
+        model.addAttribute("rsvpList", service.retrieveAllRSVPs());
+        return "view";
     }
     
 }
